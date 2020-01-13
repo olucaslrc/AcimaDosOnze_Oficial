ae0dac0349f19dc2634b823e6bbe516bef3129b9
Lucas Ribeiro da Costa <lucas.costa@benner.com.br>
2020-01-13 15:04:47 -0300

    Alterando datetime de expiração do token.

--- AuthenticationSector/Services/UserService.cs
+++ AuthenticationSector/Services/UserService.cs
@@ -56,7 +56,7 @@ namespace AcimaDosOnze_Oficial.AuthenticationSector.Services
                         new Claim(ClaimTypes.Name, user.Id.ToString()),
                         new Claim(ClaimTypes.Role, user.Role)
                     }),
-                    Expires = DateTime.UtcNow.AddDays(30),
+                    Expires = DateTime.UtcNow.AddMinutes(30),
                     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
                 };
                 var token = tokenHandler.CreateToken(tokenDescriptor);
